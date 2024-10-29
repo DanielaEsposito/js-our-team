1ea19275508ad82fefcdd9c9838276bdbe27aa06
DanielaEsposito <esposito.daniela9817@gmail.com>
2024-10-29 14:32:06 +0100

    add img

---
+++ main.js
@@ -0,0 +1,39 @@
+const ourTeam= [
+    {
+        name:"Marco",
+        role:"Designer",
+        email:"marcobianchi@team.com",
+        image:"male1.png"
+    },
+    {
+        name:"Laura Rossi",
+        role:"Front-end Developer",
+        email:"laurarossi@team.com",
+        image:"female1.png"
+    },
+    {
+        name:"Giorgio Verdi",
+        role:"Back-endDeveloper",
+        email:"giorgioverdi@team.com",
+        image:"male2.png"
+    },
+    {
+        name:"Marta Ipsum",
+        role:"SEO Specialist",
+        email:"laurarossi@team.com",
+        image:"female2.png"
+    },
+    {
+        name:"Roberto Lorem",
+        role:"SEO Specialist",
+        email:"robertolorem@team.com",
+        image:"male3.png"
+    },
+    {
+        name:"Marta Ipsum",
+        role:"SEO Specialist",
+        email:"laurarossi@team.com",
+        image:"female3.png"
+    }
+]
+
